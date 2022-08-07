2df2339d1022e5b924f9058df7d26bbb6665f90d
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-07 12:25:48 +0000

    [Update]サイドバー作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path #仮
   end
 
   def show
@@ -7,4 +14,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
