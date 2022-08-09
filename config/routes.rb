d83c16f5713645903b04d5643a61449e8d6a508c
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-09 14:56:37 +0000

    [Update]book_show完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:index, :show, :edit, :update, :create, :destory]
+  resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
   get 'homes/about'
